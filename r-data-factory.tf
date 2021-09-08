095418219fec4ae387fbaa9c10e995b46d99b3f1
bjanik <bartlomiej.janik@hotmail.fr>
2021-09-08 15:40:25 +0200

    AZ-563: Fix tricky format issue

--- r-data-factory.tf
+++ r-data-factory.tf
@@ -37,7 +37,7 @@ resource "azurerm_data_factory" "main_data_factory" {
 
     content {
       account_name    = vsts_configuration.value["account_name"]
-      branch_name     = vsts_configuration.value["branch_name"] 
+      branch_name     = vsts_configuration.value["branch_name"]
       project_name    = vsts_configuration.value["project_name"]
       repository_name = vsts_configuration.value["repository_name"]
       root_folder     = vsts_configuration.value["root_folder"]
